582cc1ddac0513cd9c6139f15645d80bc30d2e7d
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-09-07 20:03:13 +0800

    Implement UI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello!");
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
